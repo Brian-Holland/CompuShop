d7e1ebd211f2c5c2f26506849f8faeca3064d86e
Brian <brian.holland.pr@gmail.com>
2020-11-05 00:53:09 -0800

    setup backend and connected to front

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+//initialize dotenv
+dotenv.config();
+
+//initialize express
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
+//set port from .env file or default 5000
+const PORT = process.env.PORT || 5000;
+
+//listen on defined port and log status
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
