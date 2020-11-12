3a16336492b8a66d0f44a621eccca4f46ade2ad9
Brian <brian.holland.pr@gmail.com>
2020-11-11 21:47:25 -0800

    added mongodb and mongoose with schemas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 //initialize dotenv
 dotenv.config();
+
+connectDB();
 
 //initialize express
 const app = express();
